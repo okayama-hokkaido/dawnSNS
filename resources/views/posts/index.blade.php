14803c763edbd3ebafdf17462b79e6a04387dac8
wata9shi <wata9shi@gmail.com>
2021-05-20 21:44:26 +0900

    index.phpの投稿フォームを形だけ作った。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,5 +2,13 @@
 
 @section('content')
 <h2>機能を実装していきましょう。</h2>
-
+<!-- 投稿フォーム -->
+<div id=PostInput>
+  <img src="images/dawn.png" alt="icon">
+  <form method=post action="/postForm">
+    <input type="text" name="new_post" value="何を呟こうか...?">
+  </form>
+  <img src="images/post.png" alt="button">
+</div>
+<!-- 投稿一覧 -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,6 @@ Route::get('/profile','UsersController@profile');
 
 Route::get('/search','UsersController@index');
 
+
 Route::get('/follow-list','PostsController@index');
 Route::get('/follower-list','PostsController@index');
-
-
-
